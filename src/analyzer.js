28b95f7190001cf1992c57cf974c3a6672a0d791
Grecia Fragoso <naomistereo96@gmail.com>
2023-07-14 14:40:34 -0500

    cambio del onclick por addEventListener function

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,80 @@
-const analyzer = {  
+export let analyzer = {  
+  imprimirEnConsola: () => {
+    console.log('Hola')},
+
   getWordCount: (text) => {
+    let arrayWords = text.split(' ');
+    let cleanBlankSpaces = arrayWords.filter(item => item.trim() != '');
+    let wordCount = cleanBlankSpaces.length
+    //let cleanSideSpaces = text.trim()
+    //let arrayWords = cleanSideSpaces.split(' '); //matriz donde cada miembro es una palabra (se separan con espacio)
+    //let wordCount = arrayWords.length
+    document.getElementById('word-count').textContent = 'Palabras totales: ' + wordCount
+    return wordCount;
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCount: (text) => {
+    let characterCount = text.length;
+    document.getElementById('character-count').textContent = 'Caracteres totales: ' + characterCount
+    return characterCount;
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
   getCharacterCountExcludingSpaces: (text) => {
+    let textArray = text.split('');
+    let cleanAllBlankSpaces = textArray.filter(item => item.trim() != '')
+    let characterCountExcludingSpaces = cleanAllBlankSpaces.length
+    //let textWithoutSpaces = text.replace(/^\s+|\s+$/gm,'');
+    //let characterCountExcludingSpaces = textWithoutSpaces.length;
+    document.getElementById('character-no-spaces-count').textContent = 'Caracteres totales sin espacios: ' + characterCountExcludingSpaces
+    return characterCountExcludingSpaces;
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {   
+    let arrayWords = text.split(' ')
+    let cleanBlankSpaces = arrayWords.filter(item => item.trim() != '');
+    let arrayLength = cleanBlankSpaces.length
+    console.log(arrayLength)
+    let sumLength = 0
+    for (let i = 0; i < arrayLength; i++) {
+      sumLength = sumLength + cleanBlankSpaces[i].length
+      console.log(sumLength);
+    };
+    let avgLength = sumLength / arrayLength
+    document.getElementById('word-length-average').textContent = 'Longitud promedio de palabra: ' + avgLength;
+    return avgLength;
+    },
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
-  },
+  
   getNumberCount: (text) => {
+    let arrayWords = text.split(' ');
+    let cleanAllBlankSpaces = arrayWords.filter(item => item.trim() != '');
+    let arrayLength = cleanAllBlankSpaces.length
+    for (let i = 0; i <= arrayLength; i++) {
+      cleanAllBlankSpaces[i] = Number(cleanAllBlankSpaces[i])
+    };
+    let onlyNumbers = cleanAllBlankSpaces.filter(item => !isNaN(item));
+    let arrayNumbersLength = onlyNumbers.length;
+    document.getElementById('number-count').textContent = 'Cantidad de números en el texto: ' + arrayNumbersLength;
+
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberSum: (text) => {
+    let arrayWords = text.split(' ');
+    let cleanAllBlankSpaces = arrayWords.filter(item => item.trim() != '');
+    let arrayLength = cleanAllBlankSpaces.length;
+    for (let i = 0; i <= arrayLength; i++) {
+      cleanAllBlankSpaces[i] = Number(cleanAllBlankSpaces[i])
+    };
+    let onlyNumbers = cleanAllBlankSpaces.filter(item => !isNaN(item));
+    let arrayNumbersLength = onlyNumbers.length;
+    let sumNumbers = 0;
+    for (let j = 0; j < arrayNumbersLength; j++) {
+      sumNumbers = parseInt(sumNumbers + onlyNumbers[j]);
+      console.log(sumNumbers);
+    }
+    document.getElementById('number-sum').textContent = 'Suma de todos los números en el texto: ' + sumNumbers;
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
-  },
-};
+}, 
+}
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import analyzer from './analyzer.js';
-
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
-let texto_usuario=documento.getElementById('user-input')
-console.log(texto_usuario)
+let userText = document.getElementById('user-input');
+userText.addEventListener('keyup',() => {
+    let text = userText.value
+    console.log(text)
+    analyzer.getWordCount(text);
+    analyzer.getCharacterCount(text);
+    analyzer.getCharacterCountExcludingSpaces(text);
+    analyzer.getAverageWordLength(text);
+    analyzer.getNumberCount(text);
+    analyzer.getNumberSum(text);
+})
+
+let cleanTextContent = document.getElementById('clean-button');
+cleanTextContent.addEventListener('click', () => {
+    userText.value = '';
+})
+
+/*function cleanContent(){
+    //let content = document.getElementById('user-input');
+    //content.value = '';
+    text.value = '';
+};*/
