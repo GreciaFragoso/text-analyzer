93e7f737a10c34a590bf9255c1c540fef3c16110
Grecia Fragoso <naomistereo96@gmail.com>
2023-07-14 13:44:17 -0500

    Onclick button version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+let texto_usuario=documento.getElementById('user-input')
+console.log(texto_usuario)
